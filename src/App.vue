dc340d4e18037ffdb85e1d1d4e26bb8369c55bd4
Sreethan Reddy <chintagarisaisreethanreddy@gmail.com>
2025-08-01 10:59:41 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Login />
+</template>
+
+<script>
+import Login from './components/Login_In.vue'
+
+export default {
+  components: {
+    Login
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Login_In.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="div_1">
+    <h1>Username</h1>
+    <input type="text" v-model="username" placeholder="Enter Username" />
+    <h1>Password</h1>
+    <input type="password" v-model="password" placeholder="Enter Password" />
+    <br /><br /><br /><br />
+    <button @click="login">Login</button>
+    <br />
+    <h1>{{ loginSuccessful }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const username = ref('')
+const password = ref('')
+const loginSuccessful = ref('')
+
+function login() {
+  if (username.value === 'Sreethan' && password.value === '1234') {
+    loginSuccessful.value = 'Login Successful'
+  } else {
+    loginSuccessful.value = 'Login Unsuccessful, Try again'
+  }
+}
+</script>
+
+<style scoped>
+.div_1 {
+  text-align: center;
+}
+</style>
